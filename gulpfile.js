4a9086c420f358d277014083f935ddcbaf18ae99
Nazmul Alam <nalam@fexco.com>
2020-04-14 16:29:46 +0000

    added gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,16 @@
 var gulp = require('gulp');
 var bs = require('browser-sync').create(); // create a browser sync instance.
+var reload      = bs.reload;
 
-gulp.task('browser-sync', function() {
+gulp.task('default', function () {
+
+    // Serve files from the root of this project
     bs.init({
         server: {
             baseDir: "./"
         }
     });
+
+    gulp.watch("**/*.css").on("change", reload);
+    gulp.watch("*.html").on("change", reload);
 });
