c397d5274c00c70cbbb9173c8d4ecf1f08dc24b8
Nazmul Alam <nalam@fexco.com>
2020-05-03 14:50:07 +0100

    formatted files

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,12 @@
 var gulp = require('gulp');
 var bs = require('browser-sync').create(); // create a browser sync instance.
-var reload      = bs.reload;
-
+var reload = bs.reload;
 gulp.task('default', function () {
-
     // Serve files from the root of this project
     bs.init({
         server: {
             baseDir: "./"
         }
     });
-
     gulp.watch(["*.html", "assets/css/*.css"]).on("change", reload);
 });
